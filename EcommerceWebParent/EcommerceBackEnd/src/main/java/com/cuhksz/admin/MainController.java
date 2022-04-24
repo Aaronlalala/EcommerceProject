3268085990fe131b0528372d6f75c53ac93a7094
Aaron啦啦啦 <46953357+Aaronlalala@users.noreply.github.com>
2022-04-24 11:26:18 +0800

    Setup BackEnd
    1. Created a submodule called "EcommerceBackEnd" which is used for backend development.
    2. Tried some test cases to display a admin homepage. (but failed)

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/MainController.java
@@ -0,0 +1,14 @@
+package com.cuhksz.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+// Handle the HTTP GET request to the admin page of the applicaiton.
+    @GetMapping("/")
+    public String viewHomePage() {
+        return "index";
+    }
+}
