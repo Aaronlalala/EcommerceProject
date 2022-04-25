53fdae8b59e523b9368571c37e8b7ff7f36cc954
Aaron啦啦啦 <46953357+Aaronlalala@users.noreply.github.com>
2022-04-25 19:30:25 +0800

    Setup FrontEnd module

--- EcommerceWebParent/EcommerceFrontEnd/src/main/java/com/Ecommerce/website/EcommerceFrontEndApplication.java
+++ EcommerceWebParent/EcommerceFrontEnd/src/main/java/com/Ecommerce/website/EcommerceFrontEndApplication.java
@@ -1,14 +1,13 @@
-package com.cuhksz.admin;
+package com.Ecommerce.website;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-// Check the test page at http://localhost:8080/EcommerceAdmin/
-public class EcommerceBackEndApplication {
-
+// Check the test page at http://localhost/Ecommerce/
+public class EcommerceFrontEndApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EcommerceBackEndApplication.class, args);
+        SpringApplication.run(EcommerceFrontEndApplication.class, args);
     }
 }
