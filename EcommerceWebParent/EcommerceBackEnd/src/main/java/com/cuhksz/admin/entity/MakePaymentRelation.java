696fe074d11e476e0c0f472f11377de2dd344890
134arg <xen134@outlook.com>
2022-05-09 20:37:34 +0800

    backend: add MakePaymentRelation

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/entity/MakePaymentRelation.java
@@ -0,0 +1,31 @@
+package com.cuhksz.admin.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "make_payment_relation")
+public class MakePaymentRelation {
+    @Id
+    private Integer paymentId;
+
+    @Id
+    private Integer orderId;
+
+    public Integer getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(Integer paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+}
---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/MakePaymentRelationRepository.java
@@ -0,0 +1,7 @@
+package com.cuhksz.admin.repository;
+
+import com.cuhksz.admin.entity.MakePaymentRelation;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MakePaymentRelationRepository extends CrudRepository<MakePaymentRelation, Integer> {
+}
