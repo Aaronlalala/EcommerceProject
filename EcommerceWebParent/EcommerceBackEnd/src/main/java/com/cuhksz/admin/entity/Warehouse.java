dfb995b070a94215f8e2f47933dda14d478f3eb5
134arg <xen134@outlook.com>
2022-05-09 20:21:49 +0800

    backend: revise the design of warehouse

--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/entity/Warehouse.java
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/entity/Warehouse.java
@@ -10,9 +10,6 @@ public class Warehouse {
     private Integer warehouseId;
 
     private int manager;
-    private int regionId;
-    private int productId;
-    private int quantity;
     private float rent;
 
     public int getManager() {
@@ -21,30 +18,6 @@ public class Warehouse {
 
     public void setManager(int manager) {
         this.manager = manager;
-    }
-
-    public int getRegionId() {
-        return regionId;
-    }
-
-    public void setRegionId(int regionId) {
-        this.regionId = regionId;
-    }
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 
     public float getRent() {
