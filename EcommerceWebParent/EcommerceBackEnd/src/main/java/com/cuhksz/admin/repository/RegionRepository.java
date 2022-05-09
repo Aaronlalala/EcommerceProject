b2a74c6614e6b79bf6be9d5c833169357159796e
134arg <xen134@outlook.com>
2022-05-09 20:18:25 +0800

    backend: delete Region

--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cuhksz.admin.repository;
-
-import com.cuhksz.admin.entity.Region;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RegionRepository extends CrudRepository<Region, Integer> {
-}
