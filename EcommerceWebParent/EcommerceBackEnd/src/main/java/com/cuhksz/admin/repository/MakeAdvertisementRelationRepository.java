11e2de211f81d095e18fa7a8d878955ba3506701
134arg <xen134@outlook.com>
2022-05-09 20:39:27 +0800

    backend: add MakeAdvertisementRelation table

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/MakeAdvertisementRelationRepository.java
@@ -0,0 +1,7 @@
+package com.cuhksz.admin.repository;
+
+import com.cuhksz.admin.entity.MakeAdvertisementRelation;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MakeAdvertisementRelationRepository extends CrudRepository<MakeAdvertisementRelation, Integer> {
+}
