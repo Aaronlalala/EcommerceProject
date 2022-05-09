2ac7186082f74c9593db686b376e0ec921b69c49
134arg <xen134@outlook.com>
2022-05-09 20:21:01 +0800

    backend: revise the design of Shipping

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/ShippingRelationRepository.java
@@ -0,0 +1,7 @@
+package com.cuhksz.admin.repository;
+
+import com.cuhksz.admin.entity.ShippingRelation;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ShippingRelationRepository extends CrudRepository<ShippingRelation, Integer> {
+}
