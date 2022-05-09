80608a5239efa765368a8081ac75dda11d57ca6e
134arg <xen134@outlook.com>
2022-05-09 22:22:20 +0800

    backend: add query function for User by email

--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/UserRepository.java
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.cuhksz.admin.repository;
 import com.cuhksz.admin.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findUserByEmail(String email);
 }
