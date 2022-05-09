0763fe6d792d9e03928f9f42bcbf1e95e0c31aef
134arg <xen134@outlook.com>
2022-05-09 22:22:43 +0800

    backend: implement basic login / register function

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/controller/form/RegisterUser.java
@@ -0,0 +1,40 @@
+package com.cuhksz.admin.controller.form;
+
+public class RegisterUser {
+    private String name;
+    private String email;
+    private String password;
+    private String identity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+}
