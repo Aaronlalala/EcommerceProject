d7134c790ff57fab6de79c1ee6ab9f5e911dcb09
134arg <xen134@outlook.com>
2022-05-09 20:15:57 +0800

    backend: delete OrderItem

--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cuhksz.admin.repository;
-
-import com.cuhksz.admin.entity.OrderItem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderItemRepository extends CrudRepository<OrderItem, Integer> {
-}
