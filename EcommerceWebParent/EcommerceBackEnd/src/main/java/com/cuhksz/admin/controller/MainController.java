56b09218bd9e19be50ce4944690ce36881ea74ef
134arg <xen134@outlook.com>
2022-05-09 22:22:43 +0800

    backend: implement basic login / register function

--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/controller/MainController.java
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/controller/MainController.java
@@ -1,19 +1,22 @@
 package com.cuhksz.admin.controller;
 
+import com.cuhksz.admin.controller.form.LoginUser;
+import com.cuhksz.admin.controller.form.RegisterUser;
 import com.cuhksz.admin.entity.User;
+import com.cuhksz.admin.repository.MakeAdvertisementRelationRepository;
 import com.cuhksz.admin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 import java.sql.Date;
+import java.util.Optional;
 
 @Controller
 public class MainController {
     @Autowired
     private UserRepository userRepository;
+    private MakeAdvertisementRelationRepository adRelationRepository;
 
 // Handle the HTTP GET request to the admin page of the application.
     @GetMapping(value="/homepage")
@@ -31,6 +34,30 @@ public class MainController {
         n.setCreatedAt(new Date(System.currentTimeMillis()));
         userRepository.save(n);
         return "Saved";
+    }
+
+    @PostMapping(path="/register")
+    public @ResponseBody Object registerUser(@ModelAttribute RegisterUser formData) {
+        User n = new User();
+        n.setEmail(formData.getEmail());
+        n.setPassword(formData.getPassword());
+        n.setFullName(formData.getName());
+        n.setBankAccount("");
+        n.setCreatedAt(new Date(System.currentTimeMillis()));
+        return userRepository.save(n);
+    }
+
+    @PostMapping(path = "/login")
+    public @ResponseBody Object loginUser(@ModelAttribute LoginUser formData) {
+        Optional<User> res = userRepository.findUserByEmail(formData.getEmail());
+        if (!res.isEmpty()) {
+            if (res.get().checkPassword(formData.getPassword())) {
+                return "Success";
+            } else {
+                return "Wrong password";
+            }
+        }
+        return "No such user";
     }
 
     @GetMapping("/list")
