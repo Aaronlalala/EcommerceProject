80a2284e9f19c39d29c41cf23b7732b456608933
134arg <xen134@outlook.com>
2022-05-09 20:29:51 +0800

    backend: add MakeReviewRelation table

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/MakeReviewRelationRepository.java
@@ -0,0 +1,7 @@
+package com.cuhksz.admin.repository;
+
+import com.cuhksz.admin.entity.MakeReviewRelation;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MakeReviewRelationRepository extends CrudRepository<MakeReviewRelation, Integer> {
+}
