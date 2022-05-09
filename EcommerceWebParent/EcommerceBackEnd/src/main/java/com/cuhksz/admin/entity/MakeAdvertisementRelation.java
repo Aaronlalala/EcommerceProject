84a9be82491707de272fd9531411dca52671bbfb
134arg <xen134@outlook.com>
2022-05-09 20:39:27 +0800

    backend: add MakeAdvertisementRelation table

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/entity/MakeAdvertisementRelation.java
@@ -0,0 +1,42 @@
+package com.cuhksz.admin.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "make_advertisement_relation")
+public class MakeAdvertisementRelation {
+    @Id
+    private Integer adId;
+
+    @Id
+    private Integer adminId;
+
+    @Id
+    private Integer productId;
+
+    public Integer getAdId() {
+        return adId;
+    }
+
+    public void setAdId(Integer adId) {
+        this.adId = adId;
+    }
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+}
