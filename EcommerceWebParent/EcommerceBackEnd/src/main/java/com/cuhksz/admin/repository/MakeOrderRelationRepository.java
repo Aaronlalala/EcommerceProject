c0c8d61e0ffdac96c6f49dae72d5a6b654d3b182
134arg <xen134@outlook.com>
2022-05-09 20:15:10 +0800

    backend: revise the design of Order

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/MakeOrderRelationRepository.java
@@ -0,0 +1,7 @@
+package com.cuhksz.admin.repository;
+
+import com.cuhksz.admin.entity.MakeOrderRelation;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MakeOrderRelationRepository extends CrudRepository<MakeOrderRelation, Integer> {
+}
--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cuhksz.admin.repository;
-
-import com.cuhksz.admin.entity.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Integer> {
-}
