e18385ee55c22d828e5f49f0dbd03991da582643
134arg <xen134@outlook.com>
2022-05-09 20:21:01 +0800

    backend: revise the design of Shipping

--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/entity/ShippingRelation.java
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/entity/ShippingRelation.java
@@ -1,24 +1,47 @@
 package com.cuhksz.admin.entity;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
-@Table(name = "shipping")
-public class Shipping {
+@Table(name = "shipping_relation")
+public class ShippingRelation {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer shippingId;
+    private Integer id;
 
-    private int orderId;
+    private Integer orderId;
+    private Integer addressId;
     private float cost;
     private String company;
     private String type;
+    private Date shippingLimitDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public Date getShippingLimitDate() {
+        return shippingLimitDate;
+    }
+
+    public void setShippingLimitDate(Date shippingLimitDate) {
+        this.shippingLimitDate = shippingLimitDate;
+    }
 
     public int getOrderId() {
         return orderId;
     }
 
-    public void setOrderId(int orderId) {
+    public void setOrderId(Integer orderId) {
         this.orderId = orderId;
     }
 
--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/cuhksz/admin/repository/ShippingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cuhksz.admin.repository;
-
-import com.cuhksz.admin.entity.Shipping;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ShippingRepository extends CrudRepository<Shipping, Integer> {
-}
